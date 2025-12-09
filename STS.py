82f737210499b3c107840d6a1e5dd1774ec090e3
Nythron_ <105948899+Sleff1@users.noreply.github.com>
2025-12-09 17:21:51 +0100

    basic lap time display

--- STS.py
+++ STS.py
@@ -17,7 +17,7 @@ from third_party.sim_info import *
 
 
 appName = "STS"
-width, height = 800 , 800 # width and height of the app's window
+width, height = 200 , 200 # width and height of the app's window
 
 simInfo = SimInfo()
 
@@ -27,27 +27,42 @@ def acMain(ac_version):#----------------------------- App window Init
 
     # Don't forget to put anything you'll need to update later as a global variables
     global appWindow # <- you'll need to update your window in other functions.
+    global testButton
+    global label
+    global speed
 
     appWindow = ac.newApp(appName)
     ac.setTitle(appWindow, appName)
     ac.setSize(appWindow, width, height)
-
     ac.addRenderCallback(appWindow, appGL) # -> links this app's window to an OpenGL render function
+    
+    # testButton = ac.addButton(appWindow, "Test button")
+    # ac.setSize(testButton, 200, 100)
+    # def on_test_button_clicked(*args):
+    #     log("button pressed")
+    # ac.addOnClickedListener(testButton, on_test_button_clicked)
 
+    label = ac.addLabel(appWindow, "label")
+    ac.setPosition(label, 0, 100)
+    ac.setSize(label, 200, 100)
     return appName
 
-
-
-
+def log(message):
+    ac.console(message)
 
 def appGL(deltaT):#-------------------------------- OpenGL UPDATE
     """
     This is where you redraw your openGL graphics
     if you need to use them .
     """
-    pass # -> Delete this line if you do something here !
-
-
+    # Update UI elements here (called regularly by AC)
+    try:
+        # Read speed from shared memory via sim_info (reliable and avoids missing constant)
+        lastLapTime = simInfo.graphics.lastTime
+        ac.setText(label, str(lastLapTime))
+    except Exception:
+        # Keep render stable if AC isn't providing state yet
+        pass # -> Delete this line if you do something here !
 
 
 def acUpdate(deltaT):#-------------------------------- AC UPDATE
@@ -55,4 +70,8 @@ def acUpdate(deltaT):#-------------------------------- AC UPDATE
     This is where you update your app window ( != OpenGL graphics )
     such as : labels , listener , ect ...
     """
+    # Not used: updates are handled in `appGL` (render callback)
+    # If you prefer using an update callback, register it explicitly
+    # and move the UI logic here.
+    return
     pass # -> Delete this line if you do something here !
